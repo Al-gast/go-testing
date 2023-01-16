1880b03eac1812ee8be52c59915d5826b993c30b
Al-gast <agastyo2004@gmail.com>
2023-01-16 13:13:04 +0700

    testing use table driven test

--- golearn/math_test.go
+++ golearn/math_test.go
@@ -20,6 +20,45 @@ func TestAbsolute(t *testing.T) {
 
 }
 
+func TestAdd_WithTestTable(t *testing.T) {
+
+	testCases := []struct {
+		name           string
+		a, b, expected int
+	}{
+		{
+			name:     "negative and negative",
+			a:        -1,
+			b:        -1,
+			expected: -2,
+		},
+		{
+			name:     "negative and positive",
+			a:        -1,
+			b:        1,
+			expected: 0,
+		},
+		{
+			name:     "positive and positive",
+			a:        1,
+			b:        1,
+			expected: 2,
+		},
+		{
+			name:     "positive and negative",
+			a:        1,
+			b:        -1,
+			expected: 0,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			c := Add(tc.a, tc.b)
+			assert.Equal(t, tc.expected, c)
+		})
+	}
+}
+
 func TestAdd(t *testing.T) {
 
 	t.Run("negative and negative", func(t *testing.T) {
@@ -32,4 +71,13 @@ func TestAdd(t *testing.T) {
 		assert.Equal(t, -1, c)
 	})
 
+	t.Run("positive and positive", func(t *testing.T) {
+		c := Add(1, 2)
+		assert.Equal(t, 3, c)
+	})
+
+	t.Run("negative and positive", func(t *testing.T) {
+		c := Add(-1, 2)
+		assert.Equal(t, 1, c)
+	})
 }
